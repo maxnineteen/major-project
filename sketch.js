440b275819814b995e146ecbc2d61f9aa0643c0e
Zeng1602601 <1602601@sps-student.sk.ca>
2021-02-11 14:12:21 -0600

    update Major Project

--- sketch.js
+++ sketch.js
@@ -1,15 +1,54 @@
-// Project Title
-// Your Name
+// Artermis
+// Maxine Zeng
 // Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+let characterValue = {dx : 45, dy: 45};
+let characterImg;
+let ground;
+//objects = ground - height
+
+function preload(){
+  characterImg = loadImage("assets/ranger archer/run_0.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ground = height/4*3;
+  // let theCharacter = new Movement();
 }
 
 function draw() {
-  background(220);
+  background(255);
+  WASDControls;
+  image(characterImg, width/2, ground - characterValue.dy, characterValue.dx, characterValue.dy);
 }
+
+function WASDControls(){
+  if (keyIsDown(87)) {  // w
+    this.y -= this.speed;  
+  }
+  if (keyIsDown(83)) {  //s
+    this.y += this.speed;  
+  }
+  if (keyIsDown(65)) {  //a
+    this.x -= this.speed;  
+  }
+  if (keyIsDown(68)) {  //d
+    this.x += this.speed;  
+  }
+}
+
+class Attack {
+  constructor(x, y, dx, dy, speed) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.speed = speed;
+  }
+
+  move (){
+    
+  }
+}
+
