68f6f14715bd8d83125efb7b6eab65f2b387ca8a
maxnineteen <maxnineteen@gmail.com>
2021-02-24 00:48:34 -0600

    add animation

--- sketch.js
+++ sketch.js
@@ -6,63 +6,101 @@ let characterSzie =  45;
 let characterPos;
 let originalPos;
 let characterImg;
+let leftWalk;
+
 let ground;
+let jumpPad;
 //objects = ground - height
 
 function preload(){
-  characterImg = loadImage("assets/ranger archer/run_0.png");
+  leftWalk = loadImage("assets/ranger archer/run/right run.gif");
+  rightWalk = loadImage("assets/ranger archer/run/left run.gif");
+  leftIdle = loadImage("assets/ranger archer/idle/left idle_0.png");
+  rightIdle = loadImage("assets/ranger archer/idle/rigth idle_0.png");
+  rightCloseAttack = loadImage("assets/ranger archer/attack/right close attack.png");
+  leftCloseAttack = loadImage("assets/ranger archer/attack/left close attack.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ground = height/4*3;
+  jumpPad = {w:width/10, h:height/35};
   characterPos = {x: width/2, y: ground - characterSzie, speed: 5, jumpheight : height/10};
-  // let theCharacter = new Movement();
+  originalPos = characterPos.y;
+  characterImg = rightIdle;
+  console.log(originalPos);
 }
 
 function draw() {
   background(255);
-  WASDControls();
+  WASDControls();       
   fill("green");
-  rect(width/3*2, height/2, width/10, height/30);
-  image(characterImg, characterPos.x, characterPos.y, characterSzie, characterSzie);
+  rect(width/3*2, height/3*2, width/10, height/35);
+  if (characterImg === rightCloseAttack){
+    image(characterImg, characterPos.x+9, characterPos.y, 63, characterSzie);
+  }
+  else if (characterImg === leftCloseAttack){
+    image(characterImg, characterPos.x-24, characterPos.y, 63, characterSzie);
+  }
+  else{
+    image(characterImg, characterPos.x, characterPos.y, characterSzie, characterSzie);
+  }
 }
 
-// function keyPressed(){
-//   let jumpProcess = 0;
-//   if (keyCode === 32){
-//     while (jumpProcess <= characterPos.jumpheight){
-//       characterPos.y -= characterPos.speed;
-//     }
-//   }
-// }
-// function keyReleased(){
-//   if (keyCode === 32){
-//     characterPos.y += characterPos.speed;
-//   }
-// }
+function keyPressed(){
+  if (keyCode === 32){  //jump
+      characterPos.y -= characterPos.jumpheight;
+    }
+  if (keyCode === 68) {  //d left
+    characterPos.x += characterPos.speed;  
+    characterImg = leftWalk;
+  }
+  if (keyCode === 65) {  //a right
+    characterPos.x -= characterPos.speed; 
+    characterImg = rightWalk; 
+  }
+  if (keyCode === 75) {  //k
+    if (characterImg === rightWalk || characterImg === rightIdle){
+      characterImg = rightCloseAttack; 
+    } 
+    if (characterImg === leftWalk || characterImg === leftIdle){
+      characterImg = leftCloseAttack; 
+    }     
+  }
+}
+function keyReleased(){
+  if (keyCode === 32){
+    characterPos.y += characterPos.jumpheight;
+  }
+  if (keyCode === 68) {  //d
+    characterImg = rightIdle;
+  }
+  if (keyCode === 65) {  //a
+    characterImg = leftIdle; 
+  }
+  if (keyCode === 75) {  //a
+    if (characterImg === rightCloseAttack)
+      characterImg = rightIdle; 
+  }
+    if (characterImg === leftCloseAttack)
+      characterImg = leftIdle; 
+}
 
-function WASDControls(){
-  if (keyIsDown(87)) {  // w
-    characterPos.y -= characterPos.speed;
-  }
-  if (keyIsDown(83)) {  //s
-    characterPos.y += characterPos.speed;  
-  }
-  if (keyIsDown(65)) {  //a
-    characterPos.x -= characterPos.speed;  
-  }
+
+function WASDControls(){ 
   if (keyIsDown(68)) {  //d
     characterPos.x += characterPos.speed;  
+    characterImg = leftWalk;
+  }
+  if (keyIsDown(65)) {  //a
+    characterPos.x -= characterPos.speed; 
+    characterImg = rightWalk; 
   }
   if (characterPos.y >= ground - characterSzie){
     characterPos.y = ground - characterSzie;
   }
-  if (characterPos.y <= ground - characterSzie - characterPos.jumpheight){
-    characterPos.y = ground - characterSzie - characterPos.jumpheight;
-  }
 }
-
+          
 class Attack {
   constructor(x, y, dx, dy, speed) {
     this.x = x;
